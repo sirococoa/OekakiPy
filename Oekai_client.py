3946ce744d85084384843d9f6c4981e755adf4ee
sirococoa <torakia.mcml178@gmail.com>
2021-01-06 18:13:19 +0900

    [add] お絵描き機能の追加
    new_paintリストに未確定のペイントを保存し、描画

--- Oekai_client.py
+++ Oekai_client.py
@@ -7,15 +7,20 @@ class App:
     def __init__(self):
         pyxel.init(WINDOW_SIZE, WINDOW_SIZE)
         self.canvas = [[0 for _ in range(WINDOW_SIZE)] for _ in range(WINDOW_SIZE)]
+        self.new_paint = []
+        self.color = 7
         pyxel.run(self.update, self.draw)
 
     def update(self):
-        pass
+        if pyxel.btn(pyxel.MOUSE_LEFT_BUTTON):
+            self.new_paint.append((pyxel.mouse_x, pyxel.mouse_y, self.color))
 
     def draw(self):
         for x in range(WINDOW_SIZE):
             for y in range(WINDOW_SIZE):
                 pyxel.pset(x, y, self.canvas[x][y])
+        for p in self.new_paint:
+            pyxel.pset(*p)
 
 
 if __name__ == '__main__':
