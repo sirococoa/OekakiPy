1fa8f4846a03422fab34d510d2c7a16fa4bc3c9c
sirococoa <torakia.mcml178@gmail.com>
2021-01-07 15:52:05 +0900

    [add]  飛び飛びのマウス位置から線を補完するように変更

--- Oekai_client.py
+++ Oekai_client.py
@@ -18,25 +18,64 @@ class App:
         self.send_data_id = 0
         self.recv_data = ''
         self.load_canvas()
+        self.pre_mouse_x = pyxel.mouse_x
+        self.pre_mouse_y = pyxel.mouse_y
+        self.drawing = False
         pyxel.run(self.update, self.draw)
 
     def update(self):
         if pyxel.btn(pyxel.MOUSE_LEFT_BUTTON):
-            self.new_paint.append((self.send_data_id, pyxel.mouse_x, pyxel.mouse_y, self.color))
-            self.s.send(bytes(' '.join(map(str, (self.send_data_id, pyxel.mouse_x, pyxel.mouse_y, self.color))), 'utf-8'))
-            self.s.send(b'\r\n')
-            self.send_data_id += 1
-            print(len(self.new_paint))
+            if self.drawing:
+                sx, sy = pyxel.mouse_x, pyxel.mouse_y
+                gx, gy = self.pre_mouse_x, self.pre_mouse_y
+                dx, dy = sx - gx, sy - gy
+                if abs(dx) >= abs(dy):
+                    if sx < gx:
+                        for tx in range(sx, gx):
+                            if dy/dx > 0:
+                                ty = int(dy/dx*(tx - sx)+sy + 0.5)
+                            else:
+                                ty = int(dy / dx * (tx - sx) + sy - 0.5)
+                            self.draw_pixel(tx, ty)
+                    else:
+                        for tx in range(gx, sx):
+                            if dy / dx > 0:
+                                ty = int(dy / dx * (tx - sx) + sy + 0.5)
+                            else:
+                                ty = int(dy / dx * (tx - sx) + sy - 0.5)
+                            self.draw_pixel(tx, ty)
+                else:
+                    if sy < gy:
+                        for ty in range(sy, gy):
+                            if dx / dy > 0:
+                                tx = int(dx/dy*(ty - sy)+sx + 0.5)
+                            else:
+                                tx = int(dx / dy * (ty - sy) + sx - 0.5)
+                            self.draw_pixel(tx, ty)
+                    else:
+                        for ty in range(gy, sy):
+                            if dx / dy > 0:
+                                tx = int(dx / dy * (ty - sy) + sx + 0.5)
+                            else:
+                                tx = int(dx / dy * (ty - sy) + sx - 0.5)
+                            self.draw_pixel(tx, ty)
+            else:
+                self.draw_pixel(pyxel.mouse_x, pyxel.mouse_y)
+                self.drawing = True
+        else:
+            self.drawing = True
+        self.pre_mouse_x = pyxel.mouse_x
+        self.pre_mouse_y = pyxel.mouse_y
         try:
             self.recv_data += str(self.s.recv(1024), 'utf-8')
-            print("recv", self.recv_data)
+            # print("recv", self.recv_data)
             data = self.recv_data[:self.recv_data.rfind('\n')]
             self.recv_data = self.recv_data[self.recv_data.rfind('\n')+1:]
             for line in data.rstrip().split('\n'):
                 line = line.split(' ')
                 if line[0] == 'conf':
                     id = int(line[1])
-                    print("delete", id)
+                    # print("delete", id)
                     for i, x, y, c in self.new_paint:
                         if i == id:
                             self.canvas[x][y] = c
@@ -71,6 +110,11 @@ class App:
             except BlockingIOError:
                 pass
 
+    def draw_pixel(self, x, y):
+        self.new_paint.append((self.send_data_id, x, y, self.color))
+        self.s.send(bytes(' '.join(map(str, (self.send_data_id, x, y, self.color))), 'utf-8'))
+        self.s.send(b'\r\n')
+        self.send_data_id += 1
 
 if __name__ == '__main__':
     App()
