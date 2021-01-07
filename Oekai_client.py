729f49918c76e66805f8c367dd1dbc9afa17b7e0
sirococoa <torakia.mcml178@gmail.com>
2021-01-07 23:37:48 +0900

    [add] お絵描き部屋を番号指定で選べるように変更 人がいなくなった場合は初期化される

--- Oekai_client.py
+++ Oekai_client.py
@@ -5,10 +5,22 @@ import pyxel
 WINDOW_SIZE = 256
 
 
+def center(text, width):
+    '''
+    文章を中央揃えで表示する際のx座標を返す
+    :param text: 座標を得たい文章
+    :param width: 画面の幅
+    :return:
+    '''
+    TEXT_W = 4
+    return width // 2 - len(text) * TEXT_W // 2
+
+
 class App:
+    INPUT_COLL_TIME = 3
+
     def __init__(self):
         pyxel.init(WINDOW_SIZE, WINDOW_SIZE)
-        pyxel.mouse(True)
         self.canvas = [[0 for _ in range(WINDOW_SIZE)] for _ in range(WINDOW_SIZE)]
         self.new_paint = []
         self.color = 7
@@ -17,83 +29,114 @@ class App:
         self.s.setblocking(False)
         self.send_data_id = 0
         self.recv_data = ''
-        self.load_canvas()
         self.pre_mouse_x = pyxel.mouse_x
         self.pre_mouse_y = pyxel.mouse_y
         self.drawing = False
+        self.state = 'lobby'
+        self.room_number = ''
+        self.input_cool_time = 0
         pyxel.run(self.update, self.draw)
 
     def update(self):
-        if pyxel.btn(pyxel.MOUSE_LEFT_BUTTON):
-            if self.drawing:
-                sx, sy = pyxel.mouse_x, pyxel.mouse_y
-                gx, gy = self.pre_mouse_x, self.pre_mouse_y
-                dx, dy = sx - gx, sy - gy
-                if abs(dx) >= abs(dy):
-                    if sx < gx:
-                        for tx in range(sx, gx):
-                            if dy/dx > 0:
-                                ty = int(dy/dx*(tx - sx)+sy + 0.5)
-                            else:
-                                ty = int(dy / dx * (tx - sx) + sy - 0.5)
-                            self.draw_pixel(tx, ty)
-                    else:
-                        for tx in range(gx, sx):
-                            if dy / dx > 0:
-                                ty = int(dy / dx * (tx - sx) + sy + 0.5)
-                            else:
-                                ty = int(dy / dx * (tx - sx) + sy - 0.5)
-                            self.draw_pixel(tx, ty)
-                else:
-                    if sy < gy:
-                        for ty in range(sy, gy):
-                            if dx / dy > 0:
-                                tx = int(dx/dy*(ty - sy)+sx + 0.5)
-                            else:
-                                tx = int(dx / dy * (ty - sy) + sx - 0.5)
-                            self.draw_pixel(tx, ty)
-                    else:
-                        for ty in range(gy, sy):
-                            if dx / dy > 0:
-                                tx = int(dx / dy * (ty - sy) + sx + 0.5)
-                            else:
-                                tx = int(dx / dy * (ty - sy) + sx - 0.5)
-                            self.draw_pixel(tx, ty)
+        if self.state == 'lobby':
+            if self.input_cool_time == 0:
+                for key, number in ((pyxel.KEY_0, 0), (pyxel.KEY_1, 1), (pyxel.KEY_2, 2), (pyxel.KEY_4, 4), (pyxel.KEY_3, 3), (pyxel.KEY_5, 5), (pyxel.KEY_6, 6), (pyxel.KEY_7, 7), (pyxel.KEY_8, 8), (pyxel.KEY_9, 9)):
+                    if pyxel.btn(key):
+                        self.room_number = str(number) + self.room_number
+                        self.input_cool_time = self.INPUT_COLL_TIME
+                        break
             else:
-                self.draw_pixel(pyxel.mouse_x, pyxel.mouse_y)
-                self.drawing = True
-        else:
-            self.drawing = True
-        self.pre_mouse_x = pyxel.mouse_x
-        self.pre_mouse_y = pyxel.mouse_y
-        try:
-            self.recv_data += str(self.s.recv(1024), 'utf-8')
-            # print("recv", self.recv_data)
-            data = self.recv_data[:self.recv_data.rfind('\n')]
-            self.recv_data = self.recv_data[self.recv_data.rfind('\n')+1:]
-            for line in data.rstrip().split('\n'):
-                line = line.split(' ')
-                if line[0] == 'conf':
-                    id = int(line[1])
-                    # print("delete", id)
-                    for i, x, y, c in self.new_paint:
-                        if i == id:
-                            self.canvas[x][y] = c
-                    self.new_paint = [p for p in self.new_paint if p[0] != id]
-                elif line[0] == 'draw':
-                    x, y, c = map(int, line[1:])
-                    self.canvas[x][y] = c
+                self.input_cool_time -= 1
+            if len(self.room_number) == 3:
+                self.s.send(bytes(self.room_number, 'utf-8'))
+                self.s.send(b'\r\n')
+                self.state = 'room'
+                self.load_canvas()
+        elif self.state == 'room':
+            if pyxel.btn(pyxel.MOUSE_LEFT_BUTTON):
+                if self.drawing:
+                    sx, sy = pyxel.mouse_x, pyxel.mouse_y
+                    gx, gy = self.pre_mouse_x, self.pre_mouse_y
+                    dx, dy = sx - gx, sy - gy
+                    if abs(dx) >= abs(dy):
+                        if sx < gx:
+                            for tx in range(sx, gx):
+                                if dy / dx > 0:
+                                    ty = int(dy / dx * (tx - sx) + sy + 0.5)
+                                else:
+                                    ty = int(dy / dx * (tx - sx) + sy - 0.5)
+                                self.draw_pixel(tx, ty)
+                        else:
+                            for tx in range(gx, sx):
+                                if dy / dx > 0:
+                                    ty = int(dy / dx * (tx - sx) + sy + 0.5)
+                                else:
+                                    ty = int(dy / dx * (tx - sx) + sy - 0.5)
+                                self.draw_pixel(tx, ty)
+                    else:
+                        if sy < gy:
+                            for ty in range(sy, gy):
+                                if dx / dy > 0:
+                                    tx = int(dx / dy * (ty - sy) + sx + 0.5)
+                                else:
+                                    tx = int(dx / dy * (ty - sy) + sx - 0.5)
+                                self.draw_pixel(tx, ty)
+                        else:
+                            for ty in range(gy, sy):
+                                if dx / dy > 0:
+                                    tx = int(dx / dy * (ty - sy) + sx + 0.5)
+                                else:
+                                    tx = int(dx / dy * (ty - sy) + sx - 0.5)
+                                self.draw_pixel(tx, ty)
                 else:
-                    raise ValueError
-        except BlockingIOError:
-            pass
+                    self.draw_pixel(pyxel.mouse_x, pyxel.mouse_y)
+                    self.drawing = True
+            else:
+                self.drawing = True
+            self.pre_mouse_x = pyxel.mouse_x
+            self.pre_mouse_y = pyxel.mouse_y
+
+            self.color += pyxel.mouse_wheel
+            self.color = self.color % 16
+
+            try:
+                self.recv_data += str(self.s.recv(1024), 'utf-8')
+                # print("recv", self.recv_data)
+                data = self.recv_data[:self.recv_data.rfind('\n')]
+                self.recv_data = self.recv_data[self.recv_data.rfind('\n') + 1:]
+                for line in data.rstrip().split('\n'):
+                    line = line.split(' ')
+                    if line[0] == 'conf':
+                        id = int(line[1])
+                        # print("delete", id)
+                        for i, x, y, c in self.new_paint:
+                            if i == id:
+                                self.canvas[x][y] = c
+                        self.new_paint = [p for p in self.new_paint if p[0] != id]
+                    elif line[0] == 'draw':
+                        x, y, c = map(int, line[1:])
+                        self.canvas[x][y] = c
+                    else:
+                        raise ValueError
+            except BlockingIOError:
+                pass
 
     def draw(self):
-        for x in range(WINDOW_SIZE):
-            for y in range(WINDOW_SIZE):
-                pyxel.pset(x, y, self.canvas[x][y])
-        for p in self.new_paint:
-            pyxel.pset(*p[1:])
+        if self.state == 'lobby':
+            pyxel.cls(0)
+            msg = 'enter the room number!'
+            pyxel.text(center(msg, WINDOW_SIZE), WINDOW_SIZE//4, msg, 7)
+            msg = ('___' + self.room_number)[-3:]
+            pyxel.text(center(msg, WINDOW_SIZE), WINDOW_SIZE//2, msg, 7)
+        elif self.state == 'room':
+            for x in range(WINDOW_SIZE):
+                for y in range(WINDOW_SIZE):
+                    pyxel.pset(x, y, self.canvas[x][y])
+            for p in self.new_paint:
+                pyxel.pset(*p[1:])
+            if pyxel.frame_count % 10 < 5:
+                for i, j in ((0, 1), (1, 0), (0, -1), (-1, 0)):
+                    pyxel.pset(pyxel.mouse_x + i, pyxel.mouse_y + j, self.color)
 
     def load_canvas(self):
         load_success = False
@@ -106,7 +149,7 @@ class App:
                     load_success = True
                     data = data.split(' ')
                     for i in range(WINDOW_SIZE):
-                        self.canvas[i] = data[WINDOW_SIZE*i:WINDOW_SIZE*(i+1)+1]
+                        self.canvas[i] = data[WINDOW_SIZE * i:WINDOW_SIZE * (i + 1) + 1]
             except BlockingIOError:
                 pass
 
@@ -115,6 +158,7 @@ class App:
         self.s.send(bytes(' '.join(map(str, (self.send_data_id, x, y, self.color))), 'utf-8'))
         self.s.send(b'\r\n')
         self.send_data_id += 1
+
 
 if __name__ == '__main__':
     App()
--- Oekaki_server.py
+++ Oekaki_server.py
@@ -1,4 +1,4 @@
-from collections import deque
+from collections import deque, defaultdict
 import itertools
 
 from twisted.internet import reactor, task
@@ -12,6 +12,7 @@ class OekakiServer(LineReceiver):
     def __init__(self, paint_que):
         super().__init__()
         self.paint_que = paint_que
+        self.room = -1
 
     def connectionMade(self):
         print("接続受付", self.transport.client)
@@ -23,38 +24,66 @@ class OekakiServer(LineReceiver):
 
     def lineReceived(self, line):
         print(line)
-        self.paint_que.append((self, ) + tuple(map(int, str(line, 'utf-8').split(' '))))
+        if self.room == -1:
+            number = int(str(line, 'utf-8'))
+            if number >= 0:
+                self.factory.enter_room(self, number)
+        else:
+            self.paint_que[self.room].append((self, ) + tuple(map(int, str(line, 'utf-8').split(' '))))
 
 
 class OekakiFactory(Factory):
     def __init__(self):
         self.clients = []
-        self.canvas = [[0 for _ in range(WINDOW_SIZE)] for _ in range(WINDOW_SIZE)]
-        self.paint_que = deque()
+        self.canvas = defaultdict(OekakiFactory.init_canvas)
+        self.paint_que = defaultdict(deque)
+        self.visitor_number = defaultdict(int)
         self.lc = task.LoopingCall(self.loop_process)
         self.lc.start(0.1)
 
     def loop_process(self):
-        while self.paint_que:
-            painter, id, x, y, color = self.paint_que.popleft()
-            self.canvas[x][y] = color
-            for client in self.clients:
-                if client == painter:
-                    client.sendLine(bytes('conf ' + str(id), 'utf-8'))
-                else:
-                    client.sendLine(bytes('draw ' + " ".join(map(str, (x, y, color))), 'utf-8'))
+        for room, visitors in self.visitor_number.items():
+            if visitors == 0:
+                continue
+            while self.paint_que[room]:
+                painter, id, x, y, color = self.paint_que[room].popleft()
+                self.canvas[room][x][y] = color
+                for client in self.clients:
+                    if client.room != room:
+                        continue
+                    if client == painter:
+                        client.sendLine(bytes('conf ' + str(id), 'utf-8'))
+                    else:
+                        client.sendLine(bytes('draw ' + " ".join(map(str, (x, y, color))), 'utf-8'))
+
+    def enter_room(self, client, number):
+        if number < 0:
+            return -1
+        client.room = number
+        self.visitor_number[number] += 1
+        self.send_canvas(client)
+
+    def send_canvas(self, client):
+        client.sendLine(bytes(' '.join(map(str, itertools.chain.from_iterable(self.canvas[client.room]))), 'utf-8'))
 
     def clientConnectionMade(self, client):
         self.clients.append(client)
-        client.sendLine(bytes(' '.join(map(str, itertools.chain.from_iterable(self.canvas))), 'utf-8'))
 
     def clientConnectionLost(self, client):
         self.clients.remove(client)
+        if client.room >= 0:
+            self.visitor_number[client.room] -= 1
+        if self.visitor_number[client.room] == 0:
+            self.canvas[client.room] = self.init_canvas()
 
     def buildProtocol(self, addr):
         protocol = OekakiServer(self.paint_que)
         protocol.factory = self
         return protocol
+
+    @classmethod
+    def init_canvas(cls):
+        return [[0 for _ in range(WINDOW_SIZE)] for _ in range(WINDOW_SIZE)]
 
 
 factory = OekakiFactory()
